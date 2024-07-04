6f729711abd41b370b73f5b921667db23fafd3f4
SelfRaushan <160682963+SelfRaushan@users.noreply.github.com>
2024-07-04 05:19:07 +0000

    container configure

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "Raushan@google.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+//accountId = 2  //not allowed 
+accountEmail = "sdkj@com"
+accountPassword = "3252"
+accountCity = "Gaya"
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block or body of the functional 
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
