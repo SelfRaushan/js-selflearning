316213ff62aebb37b9c6dfd5ae8648b059ef2a2d
SelfRaushan <160682963+SelfRaushan@users.noreply.github.com>
2024-07-04 11:15:50 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,27 @@
 
 //alert("3+3") // we are using  nodejs ,not browser
 
+console.log(3 +3)
+
+console.log("Raushan")
+
+let name = "Raushan kumar"
+let age = 18 
+let isloggedIn = false
+
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object 
+
+
+console.log(typeof age); // undefined 
+
+console.log(typeof null); // object 
+
